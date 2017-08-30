432584dae6acd3f662b728e311cd12916a4ae7b5
Samuel Huang <samuelh2006@gmail.com>
2017-08-30 11:52:08 +1000

    Add instance shutdown waiting

--- zesty64-delete.sh
+++ zesty64-delete.sh
@@ -4,16 +4,26 @@ AMIID="ami-d511e5b3"
 PROFILE="default"
 REGION="ap-northeast-1"
 
+echo "Querying Instance..."
 INSTID=`aws --profile $PROFILE --region $REGION --output text ec2 describe-instances --filters "Name=key-name,Values=zesty64docker-key" --query 'Reservations[].Instances[0].InstanceId'`
 
-echo "Waiting for instance deletion..."
-sleep 10
-echo "aws --profile $PROFILE --region $REGION --output text ec2 terminate-instances --instance-id $INSTID"
+echo "Deleting Instance..."
 aws --profile $PROFILE --region $REGION --output text ec2 terminate-instances --instance-id $INSTID
 
 
-aws --profile $PROFILE --region $REGION --output text ec2 delete-security-group --group-name zesty64docker-sg
+for i in `seq 4 -1 0`; do
+  echo "Waiting Instance shutdown..."
+  sleep 5
+  echo "Deleteing Security Group..."
+  aws --profile $PROFILE --region $REGION --output text ec2 delete-security-group --group-name zesty64docker-sg
+  if [ "$?" -eq "0" ]; then
+    break;
+  fi
+  echo "Security Group deletion Failed. $i try left."
+done
 
 
-rm -rf zesty64docker-key.pem
+echo "Deleting SSH Key-Pair..."
 aws --profile $PROFILE --region $REGION --output text ec2 delete-key-pair --key-name zesty64docker-key
+rm -rf zesty64docker-key.pem
+
