7d7c908f1ebca6a1cf702b54eb9cc410047f3f30
Samuel <sam@tsbne>
2017-11-08 10:44:33 +1000

    Fix for SSPASS modification

--- docker-shadowsocks-libev/shadowsocks-libev.sh
+++ docker-shadowsocks-libev/shadowsocks-libev.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-PASSWORD="YOUR-SHADOWSOCKS-PASS"
+SSPASS="YOUR-SHADOWSOCKS-PASS"
 
 git clone https://github.com/EasyPi/docker-shadowsocks-libev
 cd docker-shadowsocks-libev
-sed -i 's/PASSWORD=5ouMnqPyzseL/PASSWORD=$PASSWORD/g' docker-compose.yml
+sed -i "s/PASSWORD=5ouMnqPyzseL/PASSWORD=$SSPASS/g" docker-compose.yml
 docker-compose up -d server
