b269f862263af5535c8c082c2a0fb6f881d30d7e
Samuel Huang <samuelh2006@gmail.com>
2017-12-08 08:25:17 +0000

    Alpine & Stretch docker support

---
+++ docker-alpine.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DIR=`dirname $0`
+DIR=`realpath $DIR`
+
+UID=`id -u`
+GID=`id -g`
+
+echo $DIR
+
+docker build --no-cache --build-arg UID="$UID" --build-arg GID="$GID" -f docker-vpnlaunchpad/Dockerfile.alpine --rm=true -t samuelhbne/vpnlaunchpad:alpine docker-vpnlaunchpad/
+
+mkdir -p $DIR/.vpn-launchpad/.aws
+if [ ! -f $DIR/.vpn-launchpad/.aws/config ]; then
+	echo "[default]\nregion = ap-northeast-1\noutput = json\n">>$DIR/.vpn-launchpad/.aws/config
+fi
+docker run --rm=true --name vlp-alpine -v $DIR:/home/ubuntu/vpn-launchpad -v $DIR/.vpn-launchpad/.aws:/home/ubuntu/.aws -v $DIR/.vpn-launchpad:/home/ubuntu/.vpn-launchpad -it samuelhbne/vpnlaunchpad:alpine
+
