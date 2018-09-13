e919bf2c1eb399fbc371073ff17f217d3412a15f
Samuel Huang <samuelh2006@gmail.com>
2018-09-13 11:26:00 +1000

    Add V2Ray vmess support

---
+++ docker-v2rays/docker-v2rays.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+. `pwd`/docker-v2rays.env
+
+sed -i "/EXPOSE/,/\n/ s/8000/$VMESSPORT/g" Dockerfile
+sed -i "/inbound/,/vmess/ s/8000/$VMESSPORT/g" server.json
+sed -i "/clients/,/alterId/ s/138c5e58-cd7a-455f-bef4-03016b15e6d2/$UUID/g" server.json 
+sed -i "/alterId/,/\n/ s/64/$ALTERID/g" server.json
+
+docker build --rm=true -t samuelhbne/v2rays .
+docker run --name v2rays -p $VMESSPORT:8000 -v `pwd`/server.json:/etc/v2ray/config.json -d samuelhbne/v2rays
---
+++ docker-v2rays/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+exec /usr/bin/v2ray/v2ray -config /etc/v2ray/server.json
