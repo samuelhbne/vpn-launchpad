90497acca9d0b7c4d951e0e9cadcb67aa9ba4cee
Samuel Huang <samuelh2006@gmail.com>
2018-09-17 16:01:34 +1000

    docker-v2rays config automation fixes

--- docker-v2rays/docker-v2rays.sh
+++ docker-v2rays/docker-v2rays.sh
@@ -5,10 +5,11 @@ DIR="$(cd $DIR; pwd)"
 
 . $DIR/docker-v2rays.env
 
-sed -i "/EXPOSE/,/\n/ s/8000/$VMESSPORT/g" Dockerfile
-sed -i "/inbound/,/vmess/ s/8000/$VMESSPORT/g" server.json
-sed -i "/clients/,/alterId/ s/138c5e58-cd7a-455f-bef4-03016b15e6d2/$UUID/g" server.json 
-sed -i "/alterId/,/\n/ s/64/$ALTERID/g" server.json
+sed -i "s/EXPOSE.*/EXPOSE $VMESSPORT/g" $DIR/Dockerfile
+sed -i "/inbound/,/settings/ s/\"port\":.*/\"port\": $VMESSPORT/g" $DIR/server.json
+sed -i "/inbound/,/outbound/ s/\"id\":.*/\"id\": $UUID,/g" $DIR/server.json
+sed -i "/inbound/,/outbound/ s/\"alterId\":.*/\"alterId\": $ALTERID/g" $DIR/server.json
 
-docker build --rm=true -t samuelhbne/v2rays .
+docker build --rm=true -t samuelhbne/v2rays $DIR
 docker run --name v2rays -p $VMESSPORT:8000 -v $DIR/server.json:/etc/v2ray/config.json -d samuelhbne/v2rays
+
