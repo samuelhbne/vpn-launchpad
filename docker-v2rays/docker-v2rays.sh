1e486f2231292e2e706804f6d4217badd448160a
Samuel Huang <samuelh2006@gmail.com>
2018-09-17 13:49:48 +1000

    Fix pwd issue

--- docker-v2rays/docker-v2rays.sh
+++ docker-v2rays/docker-v2rays.sh
@@ -11,4 +11,4 @@ sed -i "/clients/,/alterId/ s/138c5e58-cd7a-455f-bef4-03016b15e6d2/$UUID/g" serv
 sed -i "/alterId/,/\n/ s/64/$ALTERID/g" server.json
 
 docker build --rm=true -t samuelhbne/v2rays .
-docker run --name v2rays -p $VMESSPORT:8000 -v `pwd`/server.json:/etc/v2ray/config.json -d samuelhbne/v2rays
+docker run --name v2rays -p $VMESSPORT:8000 -v $DIR/server.json:/etc/v2ray/config.json -d samuelhbne/v2rays
