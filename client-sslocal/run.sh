e2364a54f9a0b7743bc74d5f04cb978da6444a1c
Samuel.Huang <samuelh2006@gmail.com>
2018-10-09 15:27:02 +1000

    RPI merge for docker-vlp

--- client-sslocal/run.sh
+++ client-sslocal/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-/etc/init.d/polipo restart
-cd /opt/dns-socks-proxy && ./dns_proxy
+/root/polipo/polipo -c /root/polipo/config
+cd /root/dns-tcp-socks-proxy && /root/dns-tcp-socks-proxy/dns_proxy
 exec /usr/local/bin/sslocal "$@"
--- client-sslocal/sslocal.sh
+++ client-sslocal/sslocal.sh
@@ -6,16 +6,17 @@ DIR="$(cd $DIR; pwd)"
 . $DIR/ssserver.env
 . $DIR/sslocal.env
 
-sed -i "s/EXPOSE.*/EXPOSE $SOCKSPORT $DNSPORT $HTTPPORT/g" $DIR/Dockerfile
-sed -i "s/SOCKSPORT=.*/SOCKSPORT=\"$SOCKSPORT\"/g" $DIR/Dockerfile
-sed -i "s/DNSPORT=.*/DNSPORT=\"$DNSPORT\"/g" $DIR/Dockerfile
-sed -i "s/HTTPPORT=.*/HTTPPORT=\"$HTTPPORT\"/g" $DIR/Dockerfile
+sed -i "s/EXPOSE.*/EXPOSE $SOCKSPORT $DNSPORT $HTTPPORT/g" $DIR/Dockerfile.rspi
+sed -i "s/SOCKSPORT=.*/SOCKSPORT=\"$SOCKSPORT\"/g" $DIR/Dockerfile.rspi
+sed -i "s/DNSPORT=.*/DNSPORT=\"$DNSPORT\"/g" $DIR/Dockerfile.rspi
+sed -i "s/HTTPPORT=.*/HTTPPORT=\"$HTTPPORT\"/g" $DIR/Dockerfile.rspi
 
-docker build --rm=true -t samuelhbne/sslocal $DIR
+docker build --rm=true -t samuelhbne/sslocal -f $DIR/Dockerfile.rspi $DIR
 
 BEXIST=`docker ps -a| grep sslocal|wc -l`
 if [ $BEXIST -gt 0 ]; then
         docker stop sslocal; docker rm sslocal
 fi
 
-docker run --rm=true --name sslocal -p $SOCKSPORT:$SOCKSPORT -p $DNSPORT:$DNSPORT/udp -p $HTTPPORT:$HTTPPORT -d samuelhbne/sslocal -s ${SSHOST} -p ${SSTCPPORT} -b ${SOCKSADDR} -l ${SOCKSPORT} -k "${SSPASS}" -m "${SSMETHOD}"
+echo "docker run --name sslocal -p $SOCKSPORT:$SOCKSPORT -p $DNSPORT:$DNSPORT/udp -p $HTTPPORT:$HTTPPORT -d samuelhbne/sslocal -s ${SSHOST} -p ${SSTCPPORT} -b ${SOCKSADDR} -l ${SOCKSPORT} -k \"${SSPASS}\" -m \"${SSMETHOD}\""
+docker run --name sslocal -p $SOCKSPORT:$SOCKSPORT -p $DNSPORT:$DNSPORT/udp -p $HTTPPORT:$HTTPPORT -d samuelhbne/sslocal -s ${SSHOST} -p ${SSTCPPORT} -b ${SOCKSADDR} -l ${SOCKSPORT} -k "${SSPASS}" -m "${SSMETHOD}"
