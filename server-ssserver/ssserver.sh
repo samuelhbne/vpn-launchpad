d2956da37be2aac56276b1b433bc45d2304357a2
Samuel Huang <samuelh2006@gmail.com>
2018-10-30 14:27:11 +1000

    Rewrite ssserver Dockerfile

--- server-ssserver/ssserver.sh
+++ server-ssserver/ssserver.sh
@@ -5,9 +5,5 @@ DIR="$(cd $DIR; pwd)"
 
 . $DIR/ssserver.env
 
-#sed -i "s/server_port\":.*/server_port\":\"$SSTCPPORT\",/g" $DIR/ssserver.json
-#sed -i "s/method\":.*/method\":\"$SSMETHOD\",/g" $DIR/ssserver.json
-#sed -i "s/password\":.*/password\":\"$SSPASS\",/g" $DIR/ssserver.json
-
 docker build --rm=true -t samuelhbne/ssserver $DIR
 docker run --rm=true --name ssserver -p $SSTCPPORT:$SSTCPPORT -d samuelhbne/ssserver -s 0.0.0.0 -p $SSTCPPORT -k $SSPASS -m $SSMETHOD
