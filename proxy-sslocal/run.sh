a73a04bd1294f56b94ea277f883303ba4d1f434b
Samuel Huang <samuelh2006@gmail.com>
2018-11-03 23:18:26 +1000

    Remove proxy-sslocal and server-ssserver

--- proxy-sslocal/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-/usr/bin/sslocal "$@" -d start
-/root/polipo/polipo -c /root/polipo/config
-exec /usr/bin/dnscrypt-proxy -config /etc/dnscrypt-proxy/dnscrypt-proxy.toml
--- server-ssserver/ssserver.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-DIR=`dirname $0`
-DIR="$(cd $DIR; pwd)"
-
-. $DIR/ssserver.env
-
-docker build --rm=true -t samuelhbne/ssserver $DIR
-docker run --rm=true --name ssserver -p $SSTCPPORT:$SSTCPPORT -d samuelhbne/ssserver -s 0.0.0.0 -p $SSTCPPORT -k $SSPASS -m $SSMETHOD
