53bd9f3a8e1a8631900025294e9b53b4d0485f6d
Samuel Huang <samuelh2006@gmail.com>
2018-11-20 09:27:05 +1000

    Remove '--rm=true' over samuelhbne/ssslibev

--- server-ssslibev/ssslibev.sh
+++ server-ssslibev/ssslibev.sh
@@ -6,4 +6,4 @@ DIR="$(cd $DIR; pwd)"
 . $DIR/ssslibev.env
 
 docker build --rm=true -t samuelhbne/ssslibev $DIR
-docker run --restart unless-stopped --rm=true --name ssslibev -p $SSPORT:$SSPORT -p $SSPORT:$SSPORT/udp -d samuelhbne/ssslibev -s 0.0.0.0 -s ::0 -p $SSPORT -k $SSPASS -m $SSMTHD -t 300 --fast-open -d 8.8.8.8,8.8.4.4 -u
+docker run --restart unless-stopped --name ssslibev -p $SSPORT:$SSPORT -p $SSPORT:$SSPORT/udp -d samuelhbne/ssslibev -s 0.0.0.0 -s ::0 -p $SSPORT -k $SSPASS -m $SSMTHD -t 300 --fast-open -d 8.8.8.8,8.8.4.4 -u
