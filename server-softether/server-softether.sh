9b32fb3d047aa25b3a70de25c67c58a2150bd52c
Samuel Huang <samuelh2006@gmail.com>
2018-12-17 16:08:37 +1000

    mv softether server-softether

--- server-softether/server-softether.sh
+++ server-softether/server-softether.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-#docker stop softether; docker rm softether
-
-docker run --restart unless-stopped --name softether --env-file server-softether.env --cap-add NET_ADMIN -p 500:500/udp -p 4500:4500/udp -p 1701:1701/tcp -p 1194:1194/udp -p 5555:5555/tcp -d siomiz/softethervpn
+docker run --restart unless-stopped --name server-softether --env-file server-softether.env --cap-add NET_ADMIN -p 500:500/udp -p 4500:4500/udp -p 1701:1701/tcp -p 1194:1194/udp -p 5555:5555/tcp -d siomiz/softethervpn
 
