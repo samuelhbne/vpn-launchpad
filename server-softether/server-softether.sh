c4bbc90f563b0b683856a62167c31638f0d481da
Samuel Huang <samuelh2006@gmail.com>
2018-12-24 22:37:44 +1000

    Disable SoftetherVPN deployment on non-x86(A1) instance

--- server-softether/server-softether.sh
+++ server-softether/server-softether.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
 DIR=`dirname $0`
+ARCH=`uname -m`
+
+case $ARCH in
+	x86_64|i686|i386)
+		TARGET=amd64
+		;;
+	*)
+		echo "Unsupported arch"
+		exit 255
+		;;
+esac
 
 docker run --restart unless-stopped --name server-softether --env-file $DIR/server-softether.env --cap-add NET_ADMIN -p 500:500/udp -p 4500:4500/udp -p 1701:1701/tcp -p 1194:1194/udp -p 5555:5555/tcp -d siomiz/softethervpn
-
