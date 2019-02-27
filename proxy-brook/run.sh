1a9a005f14fa3c376819110372bc4a92e87e4488
Samuel Huang <samuelh2006@gmail.com>
2019-02-27 19:58:43 +1000

    Raspbian docker.io compatable

--- proxy-brook/run.sh
+++ proxy-brook/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-/usr/bin/nohup /go/bin/brook "$@" &
+/usr/bin/nohup /root/go/bin/brook "$@" &
 /root/polipo/polipo -c /root/polipo/config
 exec /usr/bin/dnscrypt-proxy -config /etc/dnscrypt-proxy/dnscrypt-proxy.toml
