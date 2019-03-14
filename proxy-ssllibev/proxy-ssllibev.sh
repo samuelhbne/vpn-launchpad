387554e3fdfebd741a723fe6eac772fff7d4acf2
Samuel Huang <samuelh2006@gmail.com>
2019-03-14 22:56:28 +1000

    Add arm1s arch for proxy-ssllibev

--- proxy-ssllibev/proxy-ssllibev.sh
+++ proxy-ssllibev/proxy-ssllibev.sh
@@ -25,6 +25,13 @@ case $ARCH in
 		;;
 esac
 
+DOCKERVER=`docker --version|awk '{print $3}'`
+DKVERMAJOR=`echo $DOCKERVER|cut -d. -f1`
+DKVERMINOR=`echo $DOCKERVER|cut -d. -f2`
+if (("$DKVERMAJOR" < 17)) || ( (("$DKVERMAJOR" == 17)) && (("$DKVERMINOR" < 05 )) ); then
+	TARGET=$TARGET"1s"
+fi
+
 while [[ $# > 0 ]]; do
 	case $1 in
 		--from-src)
