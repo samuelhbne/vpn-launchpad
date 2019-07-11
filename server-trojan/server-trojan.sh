4860d5779604edcb541d04cbbd96b2b2488f1394
Samuel Huang <samuelh2006@gmail.com>
2019-07-11 12:14:28 +1000

    Cert obtaining exceptions.

--- server-trojan/server-trojan.sh
+++ server-trojan/server-trojan.sh
@@ -51,26 +51,30 @@ docker run \
   -p 8443:443 \
   -p 8080:80 \
   -v $DIR/config:/config \
-  --rm \
   -d linuxserver/letsencrypt
 
 CERT="$DIR/config/etc/letsencrypt/archive/$DUCKDNSDOMAIN.duckdns.org/fullchain1.pem"
 KEY="$DIR/config/etc/letsencrypt/archive/$DUCKDNSDOMAIN.duckdns.org/privkey1.pem"
-LEFT=90
+LEFT=300
 while [ $LEFT -gt 0 ]; do
 	sleep 1
 	if [ -f $CERT ] && [ -f $KEY ]; then
+		echo
 		echo "Cert obtained."
+		docker stop letsencrypt; docker rm letsencrypt
 		docker run --name server-trojan --restart unless-stopped \
 			-v $DIR/config:/config -p $TRJPORT:443 -d $IMGNAME:$TARGET \
 			-p $TRJPORT -w $TRJPASS -f $TRJFAKEDOMAIN -t $DUCKDNSTOKEN -d $DUCKDNSDOMAIN
 		exit 0
 		break;
 	fi
-	echo -en "\r$LEFT seconds left  "
+	echo -en "\r$LEFT sec left:\t"`docker logs letsencrypt 2>&1|tail -n1`"\t\t\t\t"
 	((LEFT--))
 done
 
+docker logs letsencrypt
 echo
-echo "Cert obtain failed."
+echo "Cert obtaining failed."
+echo "Please check letsencrypt log above for details."
+echo "Abort server-trojan provisioning."
 exit 255
