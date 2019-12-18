60889fb1d1dcac1c30bc9ee4da5db9e052870b86
Samuel Huang <samuelh2006@gmail.com>
2019-12-18 17:20:47 +1100

    Trim whitespace from variables

--- server-v2ray/run.sh
+++ server-v2ray/run.sh
@@ -4,16 +4,16 @@ usage() { echo "Usage: $0 [-p <port numbert>] [-u <client uuid>]" 1>&2; exit 1; 
 while getopts ":a:p:u:v:" o; do
 	case "${o}" in
 		a)
-			ALTERID=${OPTARG}
+			ALTERID="$(echo -e "${OPTARG}" | tr -d '[:space:]')"
 			;;
 		v)
-			LEVEL=${OPTARG}
+			LEVEL="$(echo -e "${OPTARG}" | tr -d '[:space:]')"
 			;;
 		p)
-			PORT=${OPTARG}
+			PORT="$(echo -e "${OPTARG}" | tr -d '[:space:]')"
 			;;
 		u)
-			UUID=${OPTARG}
+			UUID="$(echo -e "${OPTARG}" | tr -d '[:space:]')"
 			;;
 		*)
 		       	usage;
@@ -27,8 +27,8 @@ fi
 
 cd /tmp
 cp /usr/bin/v2ray/vpoint_vmess_freedom.json vvf.json
-jq "(.inbounds[] | select( .protocol == \"vmess\") | .port) |= \"$PORT\"" vvf.json >vvf.json.1
-jq "(.inbounds[] | select( .protocol == \"vmess\") | .settings.clients[0].id) |= \"$UUID\"" vvf.json.1 >vvf.json.2
-jq "(.inbounds[] | select( .protocol == \"vmess\") | .settings.clients[0].level) |= $LEVEL" vvf.json.2 >vvf.json.3
-jq "(.inbounds[] | select( .protocol == \"vmess\") | .settings.clients[0].alterId) |= $ALTERID" vvf.json.3 >server.json
+jq "(.inbounds[] | select( .protocol == \"vmess\") | .port) |= \"${PORT}\"" vvf.json >vvf.json.1
+jq "(.inbounds[] | select( .protocol == \"vmess\") | .settings.clients[0].id) |= \"${UUID}\"" vvf.json.1 >vvf.json.2
+jq "(.inbounds[] | select( .protocol == \"vmess\") | .settings.clients[0].level) |= ${LEVEL}" vvf.json.2 >vvf.json.3
+jq "(.inbounds[] | select( .protocol == \"vmess\") | .settings.clients[0].alterId) |= ${ALTERID}" vvf.json.3 >server.json
 exec /usr/bin/v2ray/v2ray -config=/tmp/server.json
