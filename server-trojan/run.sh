30b90c4e71fb0127b5cc47969e9c071ac472920b
Samuel Huang <samuelh2006@gmail.com>
2020-08-10 22:25:59 +1000

    Initial acme.sh support

--- server-trojan/run.sh
+++ server-trojan/run.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-usage() { echo "Usage: $0 -d <duckdns-domain-name> -t <duckdns-token> -w <password> [-f <fake-domain-name>]" 1>&2; exit 1; }
-while getopts ":d:f:t:w:" o; do
+usage() { echo "Usage: $0 -d <domain-name> -w <password> [-r] [-f <fake-domain-name>]" 1>&2; exit 1; }
+while getopts ":d:f:w:" o; do
 	case "${o}" in
 		d)
-			DUCKDOMAIN="$(echo -e "${OPTARG}" | tr -d '[:space:]')"
+			DOMAIN="$(echo -e "${OPTARG}" | tr -d '[:space:]')"
 			;;
 		f)
 			FAKEDOMAIN="$(echo -e "${OPTARG}" | tr -d '[:space:]')"
 			;;
-		t)
-			DUCKTOKEN="$(echo -e "${OPTARG}" | tr -d '[:space:]')"
-			;;
 		w)
 			PASSWORD="$(echo -e "${OPTARG}" | tr -d '[:space:]')"
+			;;
+		r)
+			RENEW=true
 			;;
 		*)
 		       	usage;
 	esac
 done
 
-if [ -z "${PASSWORD}" ] || [ -z "${DUCKDOMAIN}" ] || [ -z "${DUCKTOKEN}" ]; then
+if [ -z "${PASSWORD}" ] || [ -z "${DOMAIN}" ]; then
 	usage
 	exit 2
 fi
@@ -33,12 +33,20 @@ PORT=443
 
 shift $((OPTIND-1))
 
+if [ ! -f "/root/.acme.sh/${DOMAIN}/fullchain.cer" ] || [ "$RENEW" = true ]; then
+	/root/.acme.sh/acme.sh --issue --standalone --force -d ${DOMAIN}
+	if [ ! -f "/root/.acme.sh/${DOMAIN}/fullchain.cer" ]; then
+		echo "Obtian cert for ${DOMAIN} failed. Check log please."
+		exit 3
+	fi
+fi
+
 cat /trojan/examples/server.json-example  \
 	| jq " .\"local_port\" |= ${PORT} " \
 	| jq " .\"remote_addr\" |= \"${FAKEDOMAIN}\" " \
 	| jq " .\"password\"[0] |= \"${PASSWORD}\" " \
-	| jq " .\"ssl\".\"cert\" |= \"/config/etc/letsencrypt/archive/${DUCKDOMAIN}.duckdns.org/fullchain1.pem\" " \
-	| jq " .\"ssl\".\"key\" |= \"/config/etc/letsencrypt/archive/${DUCKDOMAIN}.duckdns.org/privkey1.pem\" " \
+	| jq " .\"ssl\".\"cert\" |= \"/root/.acme.sh/${DOMAIN}/fullchain.cer\" " \
+	| jq " .\"ssl\".\"key\" |= \"/root/.acme.sh/${DOMAIN}/${DOMAIN}.key\" " \
 	>/config/server.json
 
 exec /trojan/trojan /config/server.json
